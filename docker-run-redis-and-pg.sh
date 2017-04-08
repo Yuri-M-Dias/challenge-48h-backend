1ec51162b14e908a4e29240f43ddf56e146922c6
Yuri-M-Dias <yurimathe.yp@gmail.com>
2017-04-07 22:23:20 -0300

    Adds script to  run docker for the project

---
+++ docker-run-redis-and-pg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# This should be done with docker-compose, but not enough time
+
+DOCKER_REDIS_NAME=redis-48h
+DOCKER_POSTGRES_NAME=postgres-48h
+DOCKER_REDIS_PORT=6379
+DOCKER_PG_PORT=5432
+DOCKER_REDIS_HOST_STORAGE="/data/redis/48h"
+
+docker run --name "$DOCKER_POSTGRES_NAME" \
+    -p "$DOCKER_PG_PORT":"$DOCKER_PG_PORT" \
+    -e POSTGRES_PASSWORD=challenge -d postgres
+
+docker run --name "$DOCKER_REDIS_NAME" \
+    -p "$DOCKER_REDIS_PORT":"$DOCKER_REDIS_PORT" \
+    -v "$DOCKER_REDIS_HOST_STORAGE":/data \
+    -d redis redis-server --appendonly yes
+
